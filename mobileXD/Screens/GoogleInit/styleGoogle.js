efd25a38c89bb41960683d7b6f949b90cea157f9
erielilara <erieli.larad@gmail.com>
2021-08-31 20:02:53 -0500

    Ingreso a la aplicación Móvil

--- mobileXD/Screens/GoogleInit/styleGoogle.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-const styles = StyleSheet.create({
-    title: {
-        paddingTop: 200, 
-        alignItems: 'center', 
-        textAlignVertical: 'center'
-      }
-})
